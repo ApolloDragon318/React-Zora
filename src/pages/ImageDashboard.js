4b712f40395be9aa67a943c4cd21928c2df90da7
Apollo Dragon <apollodragon.soft@gmail.com>
2023-08-21 05:56:09 -0500

    feat: add-search-input

---
+++ src/pages/ImageDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchInput from '../components/SearchInput';
+
+const ImageDashboard = () => {
+  return (
+    <div>
+      <SearchInput />
+    </div>
+  )
+}
+
+export default ImageDashboard;
